9ea8923144e1ca683294d315ac72e405b5b7b6df
chhsueh <ihcaoe@gmail.com>
2014-07-15 11:11:19 +0800

    加做合成的標仔 丞宏

--- 處理中研院標音/處理標音.py
+++ 處理中研院標音/處理標音.py
@@ -20,14 +20,19 @@ from 處理中研院標音.第一步共外語處理掉 import 第一步共外語
 from 處理中研院標音.第二步拆標題漢羅佮音標 import 第二步拆標題漢羅佮音標
 from 處理中研院標音.第三步整理文本格式 import 第三步整理文本格式
 from 處理中研院標音.第四步建立句物件 import 第四步建立句物件
+from 處理中研院標音.上尾轉做HTS標仔 import 上尾轉做HTS標仔
 
 if __name__ == '__main__':
 	共外語處理掉 = 第一步共外語處理掉()
 	拆標題漢羅佮音標 = 第二步拆標題漢羅佮音標()
 	整理文本格式 = 第三步整理文本格式()
 	建立物件 = 第四步建立句物件()
+	轉做HTS標仔 = 上尾轉做HTS標仔()
 	資料 = "../TW03/"
 	os.chdir(資料)
+	這馬目錄 = os.path.dirname(os.path.abspath(__file__))
+	合成語料檔名 = os.path.join(這馬目錄, '合成語料')
+	合成語料檔案 = open(合成語料檔名, 'w')
 	for 檔名 in os.listdir("."):
 		if 檔名.endswith(".trs.html"):
 			print(檔名)
@@ -42,9 +47,11 @@ if __name__ == '__main__':
 			print(整理堆[:10])
 			臺羅堆 = 建立物件.建立(整理堆)
 			print(臺羅堆[:10])
+			轉做HTS標仔.建立(臺羅堆)
 			for 原文, 整理, 臺羅 in zip(無外語, 整理堆, 臺羅堆):
 				整理編號, 通用漢羅, 通用全羅 = 整理
 				臺羅編號, 臺羅漢羅, 臺羅全羅 = 臺羅
 				if not 原文.startswith(整理編號) or 整理編號 != 臺羅編號:
 					raise RuntimeError('{0}編號無仝'.format(整理編號))
+				print(整理編號, 臺羅全羅, file=合成語料檔案)
 			print(len(臺羅堆))

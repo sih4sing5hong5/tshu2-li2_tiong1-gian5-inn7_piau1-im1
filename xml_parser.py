b2b37c76d65d192cd5a72912e1ec4bc63475230f
pups003c <pups003c@202-169-165-182.aspublic.wlan.sinica.edu.tw>
2014-08-04 15:23:21 +0800

    attribute不會亂跳

--- xml_parser.py
+++ xml_parser.py
@@ -1,25 +1,17 @@
-from xml.dom import minidom
-from xml.dom.minidom import Node
-#file = open("data.txt", 'w')
-def remove_blanks(node):
-    for x in node.childNodes:
-        if x.nodeType == Node.TEXT_NODE:
-            if x.nodeValue:
-                x.nodeValue = x.nodeValue.strip()
-        elif x.nodeType == Node.ELEMENT_NODE:
-            remove_blanks(x)
-          
-def getText(nodelist):
-    rc = []
-    for node in nodelist:
-        if node.nodeType == node.TEXT_NODE:
-            rc.append(node.data)
-    return ''.join(rc)
-
-xml = minidom.parse('xml_test.trs')
-remove_blanks(xml)
-xml.normalize()
-Turn=xml.getElementsByTagName('Turn')
-print(xml.getAttribute('startTime'))
-"""with file('file.xml', 'w') as result:
-    result.write(xml.toprettyxml(indent = '  '))"""
+import xml.etree.ElementTree as ET
+dict={}
+tree = ET.parse('xml_test.trs')
+root = tree.getroot()
+for Epi in root.findall('Episode'):
+    for Sec in Epi.findall('Section'):
+        for Turn in Sec.findall('Turn'):
+            attribute = Turn.items()
+            if(len(attribute)==2):
+                dict.update({attribute[0][0] : attribute[0][1]})
+                dict.update({attribute[1][0] : attribute[1][1]})
+                print("start:",dict['startTime'],"end:",dict['endTime'])
+            elif(len(attribute)==3):
+                dict.update({attribute[0][0] : attribute[0][1]})
+                dict.update({attribute[1][0] : attribute[1][1]})
+                dict.update({attribute[2][0] : attribute[2][1]})
+                print("start:",dict['startTime'],"end:",dict['endTime'],"speaker:",dict['speaker'])

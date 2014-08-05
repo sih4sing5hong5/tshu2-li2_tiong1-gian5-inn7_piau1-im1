f376c6d899a9e9e6ba456133b1088c6fd644b86b
chen <chen@chen-VirtualBox>
2014-08-05 12:03:07 +0800

    new part2_unittest_input remember to change part2_unittest's input load way

--- part2_unittest.py
+++ part2_unittest.py
@@ -3,25 +3,28 @@ import trs_try as ty
 
 class CalculatorTest(unittest.TestCase):		
 	#content 有字的翻譯
-	input=open('test.trs','r',encoding='UTF-8')  
-	##content 
+	input=open('input.trs','r',encoding='UTF-8')  
+	##file's content
 	'''<Turn speaker="spk9" startTime="165.166" endTime="166.172">
 			<Sync time="165.166"/>
 			梅子呢//<ruby><rb>mui-zui4</rb><rt>梅子</rt></ruby> <ruby><rb>le3</rb><rt>呢</rt></ruby>
 			</Turn>'''
 
-	intput_1="""<Turn speaker="spk1" startTime="89.798" endTime="97.052">
+	intput_1=open('input_1.trs','r',encoding='UTF-8') 
+	##file's content 
+	'''<Turn speaker="spk1" startTime="89.798" endTime="97.052">
 	<Sync time="89.798"/>
 	ding1-gai4-la3-honnh2,li1-u3-gong1-diorh3-gong1-honnh2-
 	<Sync time="92.017"/>
 	li1-si3-piu4-gi4-huan2-neh2-honnh2-
 	<Sync time="93.783"/>
 	gong1-zit1-le2-kui2,zit3-dionn2-zi2-pior3,li1-dau4-de4-si3-kui2-sann1-mih1-kuan4,e2-zi2-pior3-
-	</Turn>"""
+	</Turn>'''
 
 	##漢羅	()
-	input_2="""<Turn speaker="spk1" startTime="0.000000" endTime="1773.844">
-	....
+	input_2=open('input_2.trs','r',encoding='UTF-8') 
+	##file's content
+	'''<Turn speaker="spk1" startTime="0.000000" endTime="1773.844">
 	<Sync time="175.477"/>
 	ze1-gan1-na1-ve3-su2-
 	<Event desc="zh" type="language" extent="begin"/>
@@ -34,7 +37,7 @@ class CalculatorTest(unittest.TestCase):
 	-le1-gong1-
 	<Sync time="179.334"/>
 	enn3-ze1-le1-gong1,a2-binn4-a2-zong1-tong4,an1-ne1-gang3-kuan4-honnh2-
-	</Turn>"""
+	</Turn>'''
 
 
 	#input	

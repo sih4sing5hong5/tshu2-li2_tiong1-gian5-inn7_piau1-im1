3b9fbb76d8365f49aeef56860103f48a9783b92b
薛丞宏 <ihcaoe@gmail.com>
2014-10-10 23:05:59 +0800

    斷字效果傷禾黑，改用斷詞 丞宏

--- 補全漢全羅/做辭典.py
+++ 補全漢全羅/做辭典.py
@@ -6,15 +6,23 @@ from 臺灣言語工具.語音合成.閩南語變調 import 閩南語變調
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 import pickle
 import os
+from 臺灣言語工具.基本元素.詞 import 詞
+from 臺灣言語工具.解析整理.詞物件網仔 import 詞物件網仔
 
 這馬所在 = os.path.dirname(__file__)
-txt = os.path.join(這馬所在, '辭典一對一.txt.gz')
-tian2 = os.path.join(這馬所在, '本調變調辭典.pickle.gz')
-語言模型 = os.path.join(這馬所在, '例句')
+辭典一對一 = os.path.join(這馬所在, '辭典一對一.txt.gz')
+斷字典 = os.path.join(這馬所在, '本調變調字典.pickle.gz')
+斷詞典 = os.path.join(這馬所在, '本調變調詞典.pickle.gz')
+斷字語言語料 = os.path.join(這馬所在, '斷字例句.txt')
+斷詞語言語料 = os.path.join(這馬所在, '斷詞例句.txt')
+# ngram-count -order 3 -interpolate -wbdiscount -unk -text 斷字例句.txt -lm 斷字例句.lm
+# ngram-count -order 3 -interpolate -wbdiscount -unk -text 斷詞例句.txt -lm 斷詞例句.lm
+斷字語言模型 = os.path.join(這馬所在, '斷字例句.lm')
+斷詞語言模型 = os.path.join(這馬所在, '斷詞例句.lm')
 
-if __name__ =='__main__':
+if __name__ == '__main__':
 	# 「｜" 丈-姆｜tiunn7-m2 」｜" 就｜to7 是｜si7 阮｜guan2 某｜boo2 的｜e5 老-母｜lau7-bu2 。｜.
-	def 檔案加入辭典(檔案, 辭典):
+	def 檔案加入字典(檔案, 辭典):
 		_分析器 = 拆文分析器()
 		_篩仔 = 字物件篩仔()
 		_變調 = 閩南語變調()
@@ -35,11 +43,49 @@ if __name__ =='__main__':
 						辭典.加詞(詞物件)
 			except:
 				pass
-	辭典 = 型音辭典(1)
-	with gzip.open(txt, 'rt') as f:
+	def 檔案加入辭典(檔案, 辭典):
+		def 變調(音):
+			拼音 = 臺灣閩南語羅馬字拼音(音)
+			if 拼音.音標 != None:
+				拼音.聲, 拼音.韻, 拼音.調 = _變調.實詞變調(拼音.聲, 拼音.韻, 拼音.調)
+				拼音.做音標()
+				return 拼音.音標
+			return 音
+		_分析器 = 拆文分析器()
+		_篩仔 = 字物件篩仔()
+		_網仔 = 詞物件網仔()
+		_變調 = 閩南語變調()
+		for line in f:
+# 			print(line)
+			try:
+				句物件 = _分析器.轉做句物件(line.strip())
+				for 詞物件 in _網仔.網出詞物件(句物件):
+					字陣列 = 詞物件.內底字
+					for 字物件 in 字陣列[:-1]:
+						字物件.型 += '/' + 字物件.音
+					for 所在 in range(len(字陣列)-1):
+						字陣列[所在].音=變調(字陣列[所在].音)
+					詞物件 = 詞(字陣列)
+					辭典.加詞(詞物件)
+					
+					字陣列[-1].音 = 變調(字陣列[-1].音)
+					詞物件 = 詞(字陣列)
+					辭典.加詞(詞物件)
+			except:
+				pass
+			
+	字典 = 型音辭典(1)
+	with gzip.open(辭典一對一, 'rt') as f:
+		檔案加入字典(f, 字典)
+	with gzip.open(斷字典, 'wb') as f:
+		pickle.dump(字典, f,
+				protocol=pickle.HIGHEST_PROTOCOL)
+		
+	辭典 = 型音辭典(4)
+	with gzip.open(辭典一對一, 'rt') as f:
 		檔案加入辭典(f, 辭典)
-# 	with open(語言模型, 'rt') as f:
-# 		檔案加入辭典(f, 辭典)
-	with gzip.open(tian2, 'wb') as f:
+	with open(斷詞語言語料, 'rt') as f:
+		檔案加入辭典(f, 辭典)
+	with gzip.open(斷詞典, 'wb') as f:
 		pickle.dump(辭典, f,
 				protocol=pickle.HIGHEST_PROTOCOL)
--- 補全漢全羅/揣全漢全羅.py
+++ 補全漢全羅/揣全漢全羅.py
@@ -1,21 +1,25 @@
-from 補全漢全羅.做辭典 import tian2
 import gzip
 import pickle
 from 臺灣言語工具.表單.肯語句連詞 import 肯語句連詞
 from 臺灣言語工具.斷詞.拄好長度辭典揣詞 import 拄好長度辭典揣詞
 from 臺灣言語工具.斷詞.連詞揀集內組 import 連詞揀集內組
 from 臺灣言語工具.解析整理.字物件篩仔 import 字物件篩仔
-import os
-這馬所在 = os.path.dirname(__file__)
-語言模型檔 = os.path.join(這馬所在, '例句.lm')
+from 補全漢全羅.做辭典 import 斷詞語言模型
+from 補全漢全羅.做辭典 import 斷字典
+from 補全漢全羅.做辭典 import 斷字語言模型
+from 補全漢全羅.做辭典 import 斷詞典
 class 揣全漢全羅:
 	辭典揣詞 = 拄好長度辭典揣詞()
 	揀集內組 = 連詞揀集內組()
 	_篩仔 = 字物件篩仔()
-	def __init__(self):
-		with gzip.open(tian2, 'rb') as f:
+	def __init__(self, 斷詞=True):
+		if 斷詞:
+			辭典, 模型 = 斷字典, 斷字語言模型
+		else:
+			辭典, 模型 = 斷詞典, 斷詞語言模型
+		with gzip.open(辭典, 'rb') as f:
 			self.辭典 = pickle.load(f)
-		self.連詞 = 肯語句連詞(語言模型檔)
+		self.連詞 = 肯語句連詞(模型)
 	def 揣(self, 變調句物件):
 		揣詞結果 = self.辭典揣詞.揣詞(self.辭典, 變調句物件)
 		揣著句物件 = 揣詞結果[0]

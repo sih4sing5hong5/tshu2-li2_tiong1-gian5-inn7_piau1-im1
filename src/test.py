c5920601cb9ed2465fb7b31fb4c72a01b31005d1
薛丞宏 <ihcaoe@gmail.com>
2014-12-22 23:15:33 +0800

    台掉用袂著的檔案　丞宏

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse('123.xml')
-root = tree.getroot()
-
-for country in root.findall('country'):
-    rank = country.find('rank').text
-    name = country.get('name')
-    print(name, rank)

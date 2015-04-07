59e9c5494b67a9855f88f820de824e4f16fb5fee
joseyarashio <joseyarashio@gmail.com>
2015-04-07 16:38:31 +0800

    新增ALEN，Neighbor

--- EDU/Cal_Similarity.py
+++ EDU/Cal_Similarity.py
@@ -11,7 +11,7 @@ def cal_differ(str1,str2):
     return d
 
 if __name__ == '__main__':
-    openfile=open('Trans005/Trans_Combine009.trs.txt','rt')
+    openfile=open('Trans006/Trans_Combine003.trs.txt','rt')
     writefile=open('結果.txt',"wt")
     
     rate=0
